566a45de2dc02f3e99c20631c7de4b4d12f7d630
Corey Mayo <cmayo@gatech.edu>
2010-04-11 15:30:52 -0400

    setup maven

---
+++ src/main/java/com/cs3240/parsergenerator/App.java
@@ -0,0 +1,13 @@
+package com.cs3240.parsergenerator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

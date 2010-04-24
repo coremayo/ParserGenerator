05ed25ac1cc0a507726bfa44d92a23fef5a2731b
bobby <jking31cs@gmail.com>
2010-04-23 21:14:58 -0400

    Changed Rule to use Symbols instead of Strings

--- src/main/java/com/cs3240/parsergenerator/Domain/Rule.java
+++ src/main/java/com/cs3240/parsergenerator/Domain/Rule.java
@@ -4,18 +4,38 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Rule {
-	private List<String> theRule;
+	private List<Symbol> theRule;
 	
-	public Rule(List<String> rule) {
+	public Rule(List<Symbol> rule) {
 		theRule = rule;
 	}
 	
-	public void setRule(List<String> rule) {
+	public void setRule(List<Symbol> rule) {
 		this.theRule = rule;
 	}
 	
-	public List<String> getRule() {
+	public List<Symbol> getRule() {
 		return theRule;
+	}
+	
+	public Symbol get(final int i) {
+		return theRule.get(i);
+	}
+	
+	public void remove(final int i) {
+		theRule.remove(i);
+	}
+	
+	public void addRuleToFront(final Rule rule) {
+		//ensures ordering correctly, so that if we're adding Bb to this rule, it
+		//adds b to beginning first, then adds B to the beginning of that.
+		for (int i = rule.getRule().size() - 1; i >= 0; i--) {
+			theRule.add(0, rule.get(i));			
+		}
+	}
+	
+	public void add(final Symbol s) {
+		theRule.add(s);
 	}
 
 }

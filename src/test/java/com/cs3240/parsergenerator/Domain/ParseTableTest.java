d00af68f4bfc5b6bf6365cbd761480c11c207a58
Corey Mayo <cmayo@gatech.edu>
2010-04-23 20:26:47 -0400

    Adding test case for ParseTable.

---
+++ src/test/java/com/cs3240/parsergenerator/Domain/ParseTableTest.java
@@ -0,0 +1,62 @@
+/**
+ * 
+ */
+package com.cs3240.parsergenerator.Domain;
+
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author corey
+ *
+ */
+public class ParseTableTest extends ParseTable {
+
+	public ParseTableTest() {
+		super(null);
+	}
+	
+	@Test
+	public void testFirst() {
+		Assert.fail("Implement ME!!!");
+	}
+	
+	@Test
+	public void testFollow() {
+		Assert.fail("Implement ME!!!");
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+}

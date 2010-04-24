db159d6e4f56331157fa8475f3e6e40b741cf4cd
bobby <jking31cs@gmail.com>
2010-04-23 22:59:20 -0400

    Grammar left recursion removal is working with sample grammar file.

--- src/main/java/com/cs3240/parsergenerator/Domain/Grammar.java
+++ src/main/java/com/cs3240/parsergenerator/Domain/Grammar.java
@@ -1,6 +1,7 @@
 package com.cs3240.parsergenerator.Domain;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -15,12 +16,14 @@ import java.util.Set;
  * TODO Make it so that a nonterminal can have multiple rules.
  */
 public class Grammar {
-	private List<Symbol> listOfSymbols;
+	private List<TerminalSymbol> listOfTerminalSymbols;
+	private List<NonterminalSymbol> listOfNonterminalSymbols;
 	private Map<NonterminalSymbol, List<Rule>> rulesMap;
 	private NonterminalSymbol startRule;
 	
 	public Grammar() {
-		this.listOfSymbols = new ArrayList<Symbol>();
+		this.listOfTerminalSymbols = new ArrayList<TerminalSymbol>();
+		this.listOfNonterminalSymbols = new ArrayList<NonterminalSymbol>();
 		this.rulesMap = new HashMap<NonterminalSymbol, List<Rule>>();
 	}
 	
@@ -29,7 +32,12 @@ public class Grammar {
 	 * @param symbol The symbol to add.
 	 */
 	public void addSymbol(Symbol symbol) {
-		listOfSymbols.add(symbol);
+		if (symbol instanceof TerminalSymbol) {
+			listOfTerminalSymbols.add((TerminalSymbol) symbol);
+		} else {
+			listOfNonterminalSymbols.add((NonterminalSymbol)symbol);
+			
+		}
 	}
 	
 	/**
@@ -80,12 +88,16 @@ public class Grammar {
 	public String toString() {
 		//First, the symbols
 		StringBuilder build = new StringBuilder();
-		build.append("SYMBOLS (" + listOfSymbols.size() + "): ");
-		for (Symbol s : listOfSymbols) {
+		build.append("TERMINAL SYMBOLS (" + listOfTerminalSymbols.size() + "): ");
+		for (Symbol s : listOfTerminalSymbols) {
 			build.append(s.getName() + " ");
 		}
 		build.append("\n");
-		
+		build.append("NONTERMINAL SYMBOLS (" + listOfNonterminalSymbols.size() + "): ");
+		for (Symbol s : listOfNonterminalSymbols) {
+			build.append(s.getName() + " ");
+		}
+		build.append("\n");
 		//Now, the Rules
 		Set<NonterminalSymbol> keySetRules = rulesMap.keySet();
 		build.append("RULES: \n");
@@ -102,17 +114,23 @@ public class Grammar {
 		return build.toString();
 	}
 	
+	/**
+	 * Goes through and removes general left recursion and immediate left recursion.
+	 * 
+	 * TODO: See if this works without Left Factoring code.
+	 */
 	public void removeLeftRecursion() {
-		List<NonterminalSymbol> nonTermList = new ArrayList<NonterminalSymbol>();
-		for (Symbol sym : listOfSymbols) {
-			if (sym instanceof NonterminalSymbol) {
-				nonTermList.add((NonterminalSymbol) sym);
-			}
-		}
-		for (int i = 0; i < nonTermList.size(); i++) {
-			NonterminalSymbol ai = nonTermList.get(i);
+		//create copy of list since listOfNonterminals will be changing from immediate
+		//left recursion.
+		List<NonterminalSymbol> tempList = 
+			new ArrayList<NonterminalSymbol>(listOfNonterminalSymbols);
+		
+		//Nasty for loop of death, check firsts, removes general left recursion
+		//then goes on to remove any immediate left recursion.
+		for (int i = 0; i < tempList.size(); i++) {
+			NonterminalSymbol ai = tempList.get(i);
 			for (int j = 0; j < i; j++) {
-				NonterminalSymbol aj = nonTermList.get(j);
+				NonterminalSymbol aj = tempList.get(j);
 				List<Rule> rulesForAi = rulesMap.get(ai);
 				List<Rule> rulesForAj = rulesMap.get(aj);
 				List<Rule> rulesToRemove = new ArrayList<Rule>();
@@ -121,7 +139,7 @@ public class Grammar {
 					if (rule.get(0).equals(aj.getName())) {
 						rulesToRemove.add(rule);
 						for (Rule ruleJ : rulesForAj) {
-							Rule ruleClone = new Rule(rule.getRule());
+							Rule ruleClone = new Rule(new ArrayList<Symbol>(rule.getRule()));
 							ruleClone.remove(0);
 							ruleClone.addRuleToFront(ruleJ);
 							rulesToAdd.add(ruleClone);
@@ -138,15 +156,26 @@ public class Grammar {
 		
 	}
 
+	/**
+	 * Goes through and removes any immediate left recursion from 
+	 * the non terminal rules of ai
+	 * @param ai The Non-terminal to check on.
+	 */
 	private void immediateLeftRecursion(NonterminalSymbol ai) {
-		boolean isNeeded = false;
+		boolean isNeeded = false; //becomes true is immediate recursion occurs
 		List<Rule> rulesForAi = rulesMap.get(ai);
 		List<Rule> rulesToAdd = new ArrayList<Rule>();
 		List<Rule> rulesToRemove = new ArrayList<Rule>();
 		NonterminalSymbol newSymbol = new NonterminalSymbol(ai.getName() + "Prime");
 		List<Rule> rulesForNewSymbol = new ArrayList<Rule>();
+		/*
+		 * This goes through each rule and finds if there is any immediate
+		 * recursion.  If there is, we add that rule to be removed, and we
+		 * add a rule that removes the first symbol in the rule (should be
+		 * ai) and adds to the add our new Symbol (ai prime).
+		 */
 		for (Rule rule : rulesForAi){
-			Rule ruleClone = new Rule(rule.getRule());
+			Rule ruleClone = new Rule(new ArrayList<Symbol>(rule.getRule()));
 			if (ruleClone.get(0).equals(ai.getName())) {
 				rulesToRemove.add(rule);
 				ruleClone.remove(0);
@@ -156,6 +185,10 @@ public class Grammar {
 				continue;
 			}
 		}
+		/*
+		 * Only runs the actual code to make changes if changes are
+		 * necessary.
+		 */
 		if (isNeeded) {
 			List<Symbol> episilonRule = new ArrayList<Symbol>();
 			episilonRule.add(new TerminalSymbol("."));
@@ -164,11 +197,12 @@ public class Grammar {
 			List<Rule> newRulesForAi = new ArrayList<Rule>(rulesForAi);
 			newRulesForAi.removeAll(rulesToRemove);
 			newRulesForAi.addAll(rulesToAdd);
+			//goes through each rule and adds to end the new symbol.
 			for (Rule rule : newRulesForAi) {
 				rule.add(newSymbol);
 			}
 			rulesMap.put(ai, newRulesForAi);
-			listOfSymbols.add(newSymbol);
+			listOfNonterminalSymbols.add(newSymbol);
 			rulesMap.put(newSymbol, rulesForNewSymbol);
 		}
 	}
@@ -182,7 +216,12 @@ public class Grammar {
 	}
 
 	public Symbol getSymbol(String name) {
-		for (Symbol s : listOfSymbols) {
+		for (Symbol s : listOfNonterminalSymbols) {
+			if (s.getName().equals(name)) {
+				return s;
+			}
+		}
+		for (Symbol s : listOfTerminalSymbols) {
 			if (s.getName().equals(name)) {
 				return s;
 			}
